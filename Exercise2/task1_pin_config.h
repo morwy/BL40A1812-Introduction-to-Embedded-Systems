e48e4565c66b9451d8b4f2d036a698c141f86eb5
TuomasLattila <tuomas.lattila@outlook.com>
2026-01-18 19:13:51 +0200

    exercise 2 task 1

---
+++ Exercise2/task1_pin_config.h
@@ -0,0 +1,25 @@
+# ifndef TASK1_PIN_CONFIG_H
+# define TASK1_PIN_CONFIG_H
+# include "board_config.h"
+// / GPIO pin mappings from Arduino UNO pins to task 1 circuit
+
+# define LED_8_PORT IO_8_PORT
+# define LED_8_DIRECTION IO_8_DIRECTION
+# define LED_8_PIN IO_8_PIN
+
+# define LED_9_PORT IO_9_PORT
+# define LED_9_DIRECTION IO_9_DIRECTION
+# define LED_9_PIN IO_9_PIN
+
+# define LED_10_PORT IO_10_PORT
+# define LED_10_DIRECTION IO_10_DIRECTION
+# define LED_10_PIN IO_10_PIN
+
+# define LED_11_PORT IO_11_PORT
+# define LED_11_DIRECTION IO_11_DIRECTION
+# define LED_11_PIN IO_11_PIN
+
+// Hint : add more definitions before the last endif
+
+
+# endif
