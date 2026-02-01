9471a83b91b2ec0d8e59a2be139c3453f18aa1d2
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2026-02-01 18:41:45 +0200

    Update main.c

--- Exercise 4/Exercise4/main.c
+++ Exercise 4/Exercise4/main.c
@@ -1,53 +1,45 @@
-/*
- * Mega_UART.c
- * Source : https://appelsiini.net/2011/simple-usart-with-avr-libc/
- *          https://www.nongnu.org/avr-libc/user-manual/group__avr__stdio.html
- *          https://ww1.microchip.com/downloads/en/devicedoc/atmel-2549-8-bit-avr-microcontroller-atmega640-1280-1281-2560-2561_datasheet.pdf 
- */ 
-
-#include "mcu.h"
-
 #include <util/delay.h>
+#include <string.h>
 #include <stdio.h>
+#include <inttypes.h>
 
-#include "uart.h"
+#define LCD_MAX_STRING (32)
 
-/// There is not much we can do for now. This function will be improved in future.
-static void handle_error(uint8_t return_code)
+/**
+ * Protected write to LCD that checks that provided pointer is a valid
+ * null-terminated string.
+ * @param string Pointer to the string that should be printed
+ */
+static void write_to_lcd(const char *string)
 {
-	// Non-zero return code indicates critical fault
-	if (return_code)
+	uint8_t len = strnlen(string, LCD_MAX_STRING);
+	if (LCD_MAX_STRING == len)
 	{
-	    while(1);
+		printf("Failed to print LCD string. Too big or lacks NULL-terminator.\r\n");
+		// Since we have null-terminator we print the bad string one character at a time.
+		for (uint8_t i = 0; i < len; i++)
+		{
+			printf("%c", string[i]);
+		}
+		printf("\r\n");
+		handle_error(1);
+	}
+	else
+	{
+		printf("LCD output: '%s'\r\n", string);
+		lcd_puts(string);
 	}
 }
 
 int main(void)
 {
-    uint8_t rc = setup_uart_io();
-    handle_error(rc);
-    char username[32] = {0};
-
-    // print out using the UART. This can be accessed via terminals such as PuTTY.
-    printf("Hello World! What is your name (max 30 characters)?\r\n");
-
-    // Read username safely
-    fgets(username, sizeof(username), stdin);
-
-    for (size_t i = 0; i < sizeof(username); i++)
-    {
-		if (username[i] == 10) // if it is an LF
-		{
-			username[i] = 0; // finished the string
-			break;
-		}
-    }
-
-    while (1) 
-    {
-        printf("Hello, %s!\r\n", username);
-        _delay_ms(1000);
-    }
- 
-    return 0;
+	static char key_str[32];
+	setup();
+	uint32_t memory = 0;
+	while (1)
+	{
+		/************************************************************************/
+		/*                                                                      */
+		/************************************************************************/
+	}
 }
