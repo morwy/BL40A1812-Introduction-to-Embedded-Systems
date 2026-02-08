dbf85a417244b557eee08933ae8917deb2486654
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2026-02-08 20:01:32 +0200

    Update main.c

--- Exercise 5/Exercise5/main.c
+++ Exercise 5/Exercise5/main.c
@@ -10,6 +10,8 @@
 #include <avr/sleep.h>
 #include <stdbool.h>
 
+#define LCD_MAX_STRING (32)
+
 /// There is not much we can do for now. This function will be improved in future.
 static void handle_error(uint8_t return_code)
 {
@@ -18,6 +20,32 @@ static void handle_error(uint8_t return_code)
 	{
 		while (1)
 			;
+	}
+}
+
+/**
+ * Protected write to LCD that checks that provided pointer is a valid
+ * null-terminated string.
+ * @param string Pointer to the string that should be printed
+ */
+static void write_to_lcd(const char *string)
+{
+	uint8_t len = strnlen(string, LCD_MAX_STRING);
+	if (LCD_MAX_STRING == len)
+	{
+		printf("Failed to print LCD string. Too big or lacks NULL-terminator.\r\n");
+		// Since we have null-terminator we print the bad string one character at a time.
+		for (uint8_t i = 0; i < len; i++)
+		{
+			printf("%c", string[i]);
+		}
+		printf("\r\n");
+		handle_error(1);
+	}
+	else
+	{
+		printf("LCD output: '%s'\r\n", string);
+		lcd_puts(string);
 	}
 }
 
