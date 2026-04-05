671c7aeab7e70f9c4a7c4712284c977bbcac12f1
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2026-04-05 18:24:34 +0300

    Initial 2560 project

---
+++ Project/2560/Project2560/mcu.h
@@ -0,0 +1,13 @@
+/**
+ *  This header stores target-specific constant that relate to MCU itself 
+ *  (excluding periferals which are confiured in own headers)
+ * */
+
+#ifndef MCU_H
+#define MCU_H
+
+#ifndef F_CPU
+#define F_CPU (16000000UL)
+#endif
+
+#endif
